5c1e9ab5c032f8450bd0cfacad358900527c7bfe
sooriyadas91 <80309054+sooriyadas91@users.noreply.github.com>
2021-03-09 17:42:34 +0530

    changes

--- CWingTest/src/app/app.component.ts
+++ CWingTest/src/app/app.component.ts
@@ -4,21 +4,20 @@ Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
-})
+});
 
 export class AppComponent {
-  total: number = 100;
-  page1: number = 10
-  array = []
+  total = 100;
+  page1 = 10;
+  array = [];
   title = 'CWingTest';
   getCount() {
+    let b: any;
 
-    var b: any
-    var a: any =this.total / this.page1
+    const a: any = this.total / this.page1;
     return Math.trunc(a);
-    this.array.forEach(element => {
-      var c = 10
+    this.array.forEach((element) => {
+      const c = 10;
     });
   }
 }
-
