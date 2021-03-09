34691120756a54c5044986f4f5da79a1160ada08
sooriyadas91 <80309054+sooriyadas91@users.noreply.github.com>
2021-03-09 18:19:38 +0530

    v2

--- CWingTest/src/app/app.component.ts
+++ CWingTest/src/app/app.component.ts
@@ -14,7 +14,6 @@ export class AppComponent {
   getCount() {
     const d = 10;
     let b: any;
-
     const a: any = this.total / this.page1;
     return Math.trunc(a);
     this.array.forEach((element) => {
